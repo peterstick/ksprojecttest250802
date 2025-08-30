ebaa1d632b4459750435a703772634cd074cfa7d
pwr <dkdls0318@gmail.com>
2025-08-30 09:54:56 +0900

    문자 형태의 연산

--- operator.py
+++ operator.py
@@ -43,7 +43,25 @@ str_y = "python"
 str_z = str_x + str_y
 print("str_x+str_y : ", str_z)
 
+
 str_z = str_x*2
 print("str_x*2 : ", str_z)
 
+
+
+delayed_effect_skill_a = "" #snake case
+delayedEffectSkillA = #camelCase
+
+
+array_x = [12314, 12344]
+array_y = [123567, 213123]
+
+array_z = array_x + array_y
+print("array_x + array_y : ", array_z)
+
+array_z = array_x*2
+print("array_x *2 : ", array_z)
+
+array_z = array_x* array_y[0]
+print("array_x + array_y[0] : ", array_z)
 
