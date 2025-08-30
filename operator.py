ba9a68551e635a30e413de5cd4256b48beb82e11
pwr <dkdls0318@gmail.com>
2025-08-30 10:06:45 +0900

    문자 형태의 연산

--- operator.py
+++ operator.py
@@ -65,3 +65,22 @@ print("array_x *2 : ", array_z)
 array_z = array_x* array_y[0]
 print("array_x + array_y[0] : ", array_z)
 
+reportCard   = {
+    "국어" : 3,
+    "수학" : 2,
+    "영어" : 6,
+    "물리" : 4,
+    "화학" : 2,
+    "생명과학" : 5,
+}
+
+canApply = reportCard["국어"] <= 3 and reportCard["수학"] <= 3 and reportCard["영어"] <= 3
+print("지원가능여부 : ", canApply)
+
+#3합6
+totalScore = reportCard["국어"]+reportCard["수학"]+reportCard["영어"] <= 6
+print("3합6 : ", totalScore)
+
+#과학 영재반
+can_apply_for_the_gifted = reportCard["물리"] ==1 or reportCard["화학"] ==1 or reportCard["생명과학"] == 1
+print("영재반 지원가능여부 :", can_apply_for_the_gifted )
