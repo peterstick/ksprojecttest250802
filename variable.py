2fa07bdaee91476ebcb5a50d932ff66cdacde1fa
pwr <dkdls0318@gmail.com>
2025-08-30 11:30:09 +0900

    문자 형태의 연산

--- variable.py
+++ variable.py
@@ -19,5 +19,28 @@ print("x")
 print("배열의 길이====")
 len(x)
 
-print(x)("배열의 마지막 인덱스")
+print("배열의 마지막 인덱스")
 print(len(x)-1)
+
+
+x = {'a':100, 'b': 200} #dictionary, key:value
+print(x)
+print(x['a'])
+print(x['b'])
+
+print("dictionary 에제=======")
+x['c'] = 10
+print(x)
+print(x['c'])
+#print(x['d'])
+
+del x['a'] #딕셔너리에서 특정 기값 지우기
+print(x)
+
+
+print(x.keys())
+print(x.values())
+print(x.items())
+
+print(x.clear()) #딕셔너리 지우기
+print(x)
