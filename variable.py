865c21e2392eec2f6fde4ae8979bc801b4595456
pwr <dkdls0318@gmail.com>
2025-08-30 10:48:49 +0900

    문자 형태의 연산

---
+++ variable.py
@@ -0,0 +1,23 @@
+x = [1, 2, 3, 4, 5] # list
+
+print("slicing=========")
+print(x[2:5])  #[end_index+1]]
+print("apend=======")
+x.append(7)
+print(x)
+print("insert=============")
+x.insert(1, 9)
+print(x)
+print("remove=========")
+x.remove(1)
+print(x)
+print("9->6=========")
+x[0] = 6
+print(x)
+print("x")
+
+print("배열의 길이====")
+len(x)
+
+print(x)("배열의 마지막 인덱스")
+print(len(x)-1)
