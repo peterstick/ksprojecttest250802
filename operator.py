652e160a66772b3131b4aa5303c46c446bd6040c
pwr <dkdls0318@gmail.com>
2025-08-30 09:31:05 +0900

    숫자 형태의 연산자

---
+++ operator.py
@@ -0,0 +1,36 @@
+x= 6 #대입연산자
+y- 4
+
+
+#조건연산자
+z = x == y
+print("x == y : ", z)
+
+z = x != y
+print("x != y : ", z)
+z = x > y
+print("x > y : ", z)
+z = x >= y
+print("x >= y : ", z)
+z = x<y
+print("x < y : ", z)
+z = x<=y
+print("x <= y : ", z)
+
+#사칙연산
+z = x + y
+print("x + y : ", z)
+z = x - y
+print("x - y : ", z)
+z = x*y
+print("x * y : ", z)
+z= x / y
+print("x / y : ", z)
+
+z = x%y
+print("x % y : ", z)
+z = x // y
+print("x // y : ", z)
+
+z = x ** y
+print("x ** y : ", z)
