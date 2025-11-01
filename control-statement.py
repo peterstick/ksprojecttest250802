068f090a9a7f6a6a41cec3429d8de045fba0ba7c
pwr <dkdls0318@gmail.com>
2025-11-01 11:08:25 +0900

    조건문

---
+++ control-statement.py
@@ -0,0 +1,10 @@
+score = int(input("점수를 내놓거라 닝겐"))
+
+if score >= 80:
+    print("합격이다 닝겐")
+elif score >= 60:
+    print("기회를 한번 더 주지 닝겐")
+elif score < 0:
+    print("말같은 소리를 해라 닝겐")
+else:
+    print("강해져서 돌아와라 닝겐")
