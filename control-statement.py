58d027383e1062d493844fe6d41442e8686880b9
pwr <dkdls0318@gmail.com>
2025-11-01 11:48:14 +0900

    fibo낙지

--- control-statement.py
+++ control-statement.py
@@ -1,39 +1,50 @@
-
-
-score = int(input("점수를 내놓거라 닝겐"))
-
-
-if score < 0 and score > 100:
-    print("말같은 소리를 해라 닝겐")
-if score >= 80:
-    print("합격이다 닝겐")
-elif score >= 60:
-    print("기회를 한번 더 주지 닝겐")
-else:
-    print("강해져서 돌아와라 닝겐")
-
-
-
-#while True:
- #   user_input = input("값을내놔러ㅏ")
+# ###
 #
- #   if user_input.lower() == "z":
-  #      break
+# score = int(input("점수를 내놓거라 닝겐"))
+#
+#
+# if score < 0 and score > 100:
+#     print("말같은 소리를 해라 닝겐")
+# if score >= 80:
+#     print("합격이다 닝겐")
+# elif score >= 60:
+#     print("기회를 한번 더 주지 닝겐")
+# else:
+#     print("강해져서 돌아와라 닝겐")
+#
+#
+#
+# #while True:
+#  #   user_input = input("값을내놔러ㅏ")
+# #
+#  #   if user_input.lower() == "z":
+#   #      break
+#
+#
+# input_number = int(input("수를 내놔라"))
+# index =0
+#
+# while index < input_number:
+#     print(index)
+#     index += 1
+
+#input_number = int(input("수줘"))
+#index = 2
+
+#while index <= input_number:
+ #   if index % 2 == 0:
+#        print(index)
 
 
-input_number = int(input("수를 내놔라"))
-index =0
+fibo1 = 1
+fibo2 = 1
 
-while index < input_number:
-    print(index)
-    index += 1
+print(fibo1)
+print(fibo2)
 
-
-input_number = int(input("수줘"))
-index = 2
-
-while index <= input_number:
-    if index % 2 == 0:
-        print(index)
-    index = index + 2
+while fibo1 <= 100:
+    fibo1 = fibo1+fibo2
+    print(fibo1)
+    fibo2 = fibo1+fibo2
+    print(fibo2)
 
