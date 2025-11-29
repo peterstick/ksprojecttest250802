085d01b00005d0542ec223a54a5029f149e4d2cc
pwr <dkdls0318@gmail.com>
2025-11-29 10:34:24 +0900

    업당ㅜㄴ끝

--- playground.py
+++ playground.py
@@ -16,27 +16,67 @@ import time
 # def example_function(input_arg:int)
 
 
-cAnswer = random.randint(1, 100)
-count=1
 
 
+
+
+
+
+def updown():
+    cAnswer = random.randint(1, 100)
+    count=1
+
+
+
+    while True:
+        try:
+            user_input = int(input("한번 답해보거라 인간 : "))
+        except ValueError:
+            print("숫자를달라고")
+            continue
+
+        if user_input > cAnswer:
+            print("down")
+            count += 1
+        elif user_input < cAnswer:
+            print("up")
+            count += 1
+
+
+        if user_input == cAnswer:
+            print("정답이다 끄지라")
+            count=0
+            break
+
+
+
+def quiz():
+    print("WELCOME TO QUIZ!")
+
+
+
+
+
+def stop_watch():
+    print("WELCOME TO UP STOPWATCH")
 
 while True:
-    try:
-        user_input = int(input("한번 답해보거라 인간 : "))
-    except ValueError:
-        print("숫자를달라고")
-        continue
+    print('''
+    ================메뉴================
+    A. Up & Down 게임
+    B. 영어 낱말 맞추기
+    C. Stop watch 게임
+    Z. 프로그램 종료
+    ====================================
+    ''')
+    user_input = input("값을 입력하세요 : ")
 
-    if user_input > cAnswer:
-        print("down")
-        count += 1
-    elif user_input < cAnswer:
-        print("up")
-        count += 1
+    if user_input.lower() == "a":
+        updown()
 
-
-    if user_input == cAnswer:
-        print("정답이다 끄지라")
-        count=0
+    elif user_input.lower() == "b":
+        quiz()
+    elif user_input.lower() == "c":
+        stop_watch()
+    elif user_input.lower() == "z":
         break
